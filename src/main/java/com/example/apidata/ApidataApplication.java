d6c3985cf13fa7b0b1324d892cb46cc5477a447a
ryanda9910 <ryandaaldo@gmail.com>
2020-06-14 13:21:41 +0700

    initial commit

--- src/main/java/com/example/apidata/ApidataApplication.java
+++ src/main/java/com/example/apidata/ApidataApplication.java
@@ -14,7 +14,7 @@ public class ApidataApplication {
 		SpringApplication.run(ApidataApplication.class, args);
 	}
 
-	@GetMapping("/hello")
+	@GetMapping("/") //root
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
